d3a58332a45da6080ab0a2287b98065cb7187b0d
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-03 14:16:06 +0300

    btns

--- src/Components/Controls/Controls.tsx
+++ src/Components/Controls/Controls.tsx
@@ -1,12 +1,27 @@
 import react from 'react';
+// import '../Controls';
 
+type Props = {
+    setTimeInSeconds: Function
+}
 
-function Controls() {
+function Controls(props: Props) {
     return (
         <section>
-            
+            <button style={btnStyles}>Stop</button>
+            <button style={btnStyles}>Play</button>
+            <button style={btnStyles}>Reset</button>
         </section>
     )
+}
+
+const btnStyles = { 
+    margin: 15, 
+    border: 'none', 
+    backgroundColor: '#E5AD3D',
+    padding: '3px 12px',
+    fontSize: '1.3rem',
+    cursor: 'pointer'
 }
 
 export default Controls;
